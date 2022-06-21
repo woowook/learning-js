e564fe22aa404526cbe2b842f66cca38fac2a6bb
wook <wook@neowiz.com>
2022-06-22 07:06:42 +0900

    todolist

--- src/TodoList/todolist.js
+++ src/TodoList/todolist.js
@@ -9,24 +9,69 @@ const toDoInput = toDoForm.querySelector('input');
 // Html에서 class 이름이 toDos를 찾아서 첫번째 요소를 리턴
 const toDos = document.querySelector('.toDos');
 
-function paintToDo(toDo) {
-  // JS에서 html 요소를 생성하기 위해 createElement 사용 (li, span)
-  const li = document.createElement('li');
-  const span = document.createElement('span');
-  span.innerHTML = toDo;
-  li.append(span);
-  toDos.append(li);
+const TODOLIST = 'toDoList';
+let toDoList = [];
+
+function loadToDoList() {
+  // https://developer.mozilla.org/ko/docs/Web/API/Window/localStorage
+  const loadedToDoList = localStorage.getItem(TODOLIST);
+  if (loadedToDoList !== null) {
+    const parsedToDoList = JSON.parse(loadedToDoList);
+    for (const toDo of parsedToDoList) {
+      const { text } = toDo;
+      paintToDo(text);
+      saveToDo(text);
+    }
+  }
 }
 
-//
+function saveToDoList() {
+  localStorage.setItem(TODOLIST, JSON.stringify(toDoList));
+}
+
+function saveToDo(toDo) {
+  const toDoObject = {
+    text: toDo,
+    id: toDoList.length + 1,
+  };
+  toDoList.push(toDoObject);
+  saveToDoList();
+}
+
+function delToDo(event) {
+  const { target: button } = event;
+  const li = button.parentNode;
+  li.remove();
+  toDoList = toDoList.filter((toDo) => toDo.id !== Number(li.id));
+  saveToDoList();
+}
+
 function createToDo(event) {
   event.preventDefault();
   const toDo = toDoInput.value;
   paintToDo(toDo);
+  saveToDo(toDo);
   toDoInput.value = '';
 }
 
+function paintToDo(toDo) {
+  // JS에서 html 요소를 생성하기 위해 createElement 사용 (li, span)
+  const li = document.createElement('li');
+  const span = document.createElement('span');
+  const delButton = document.createElement('button');
+  // DelButton.innerText = 'Del';
+  // https://developer.mozilla.org/ko/docs/Web/API/Node/textContent
+  delButton.textContent = 'Del';
+  delButton.addEventListener('click', delToDo);
+  span.innerHTML = toDo;
+  li.append(span);
+  li.append(delButton);
+  li.id = toDoList.length + 1;
+  toDos.append(li);
+}
+
 function init() {
+  loadToDoList();
   toDoForm.addEventListener('submit', createToDo);
 }
 
